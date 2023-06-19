a2f81eafe20d5bed51f31baaeaa8891d4f748c2b
Shivani <122675366+shivanirai28@users.noreply.github.com>
2023-06-19 20:42:21 +0530

    INPUT ASSIGNMENT
    Input Assignment.

---
+++ JavaInput/src/SameLine.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SameLine {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner (System.in);
+        String first = scn.nextLine();
+        String last = scn.nextLine();
+        System.out.println(first + last);
+
+    }
+}
